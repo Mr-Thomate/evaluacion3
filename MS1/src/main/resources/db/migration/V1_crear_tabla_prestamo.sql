96fb0772cc3ce83ca23d60b19357dd635496fd50
Christian-Villalobos <chr.villalobos@duocuc.cl>
2026-06-24 15:24:36 -0400

    Delete MS1/src/main/resources/db/migration/V1_crear_tabla_prestamo.sql

--- MS1/src/main/resources/db/migration/V1_crear_tabla_prestamo.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE prestamo (
-    id_prestamo INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente INT NOT NULL,
-    id_libro INT NOT NULL,
-    fecha_prestamo DATE NOT NULL,
-    fecha_devolucion DATE NULL,
-    CONSTRAINT fk_prestamo_cliente 
-        FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
-);
