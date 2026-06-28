42daa3935c8cf88ce67e76599e3cc481c3e91cb0
mat-di <mata.diazs@duocuc.cl>
2026-06-27 21:30:56 -0400

    Add files via upload

---
+++ MS3/src/main/java/MS3/MS3/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package MS3.MS3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+            new Info()
+            .title("API 2026 Evaluacion 3 Biblioteca MS3")
+            .version("0.5.0")
+            .description("Api de la tercera evaluacion de nuestro proyecto de biblioteca con el segundo microservicio")
+        );
+    }
+}
+
